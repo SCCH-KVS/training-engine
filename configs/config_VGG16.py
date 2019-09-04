0a1088765c63fc1cf12bad04baf78dae905a1f01
scchwagner <hanna.wagner@scch.at>
2019-09-04 15:14:17 +0200

    [UPDATED] VGG16.

--- configs/config_VGG16.py
+++ configs/config_VGG16.py
@@ -23,15 +23,15 @@ def load_config():
     config.net = 'VGG16'
     config.training_mode = True
     config.data_set = 'MNIST'
-    config.image_size = [28, 28, 1]
-    config.lr = 0.1  # changable
+    config.image_size = [32, 32, 1]
+    config.lr = 0.01  # changable
     config.lr_decay = 0.1  # changable
-    config.ref_steps = 3  # changable
-    config.ref_patience = 3  # changable
-    config.batch_size = 32  # changable
-    config.num_epochs = 1  # changable
+    config.ref_steps = 10 # changable
+    config.ref_patience = 10  # changable
+    config.batch_size = 64  # changable
+    config.num_epochs = 10  # changable
     config.loss = 'softmax'  # changable
-    config.optimizer = 'sgd'
+    config.optimizer = 'gradient'
     config.mi_record = False
     config.gpu_load = 0.8
     config.num_classes = 10
@@ -48,12 +48,28 @@ def load_config():
     config.trainable_layers = 'all'
     config.normalize = True
     config.zero_center = True
-    config.dropout = 0.4
+    config.dropout = 0.5
     #config.chpnt2load = r'experiments/ckpnt_logs/MNIST/1566220587.1112642/1566220587.1112642_split_0'
     config.chpnt2load = ''
     config.multi_task = False
     config.cross_val = 1
     config.framework = 'tensorflow'
     config.experiment_path = None
+
+    # For no optimisation:
+    # enter range with desired value e.g. [0, 0] for continuous
+    # or fixed Value for discrete e.g. ['percent']
+    # Epochs are definded over max_amount_resources
+    config.hyperband = False
+    config.halving_proportion = 2  # min 2
+    config.max_amount_resources = 2   # no of max epochs (min 2, max 1000)
+    config.lr_range = [0.1, 0.00001]  # [0.1, 0.00001]
+    config.lr_decay_range = [0.1, 0.00001]  # [0.1, 0.00001]
+    config.ref_steps_range = [0, 1]  # 0, number of epochs /3
+    config.ref_patience_range = [0, 1]   # 0, number of epochs /3
+    config.batch_size_range = [config.num_classes, 512]  # num_classes, max capacity of memory
+    config.loss_range = ['softmax', 'sigmoid', 'margin']  # ['softmax', 'sigmoid', 'margin', 'mse', 'mse_loss']
+    config.accuracy_range = ['percent']  # ['mse', 'percent']
+    config.optimizer_range = ['adam', 'momentum', 'gradient']  # ['adam', 'momentum', 'gradient']
 
     return config
--- network/wrappers/VGG16.py
+++ network/wrappers/VGG16.py
@@ -23,7 +23,7 @@ from network.wrappers.NetworkBase import NetworkBase
 
 class VGG16(NetworkBase):
     def __init__(self, network_type, loss, accuracy, lr, framework, training, trainable_layers=None, num_filters=64,
-                 optimizer='adam', nonlin='elu', num_classes=2, dropout=0.25):
+                 optimizer='adam', nonlin='relu', num_classes=2, dropout=0.5):
         """
         VGG16 Convolutional Neural Network constructor
         :param loss:        used loss function
@@ -49,78 +49,88 @@ class VGG16(NetworkBase):
         with tf.name_scope('s_conv_1'):
             conv_1_1, batch_1_1, activ_1_1 = self._conv_bn_layer_tf(X, n_filters=self.num_filters, filter_size=3,
                                                                     is_training=self.is_training,
-                                                                    nonlin_f=self.nonlin_f,
-                                                                    name_postfix='1_1')
+                                                                    nonlin_f=self.nonlin_f, name_postfix='1_1')
+            activ_1_1 = self.nonlin_f(batch_1_1, name='activation_1_1')
             conv_1_2, batch_1_2, activ_1_2 = self._conv_bn_layer_tf(activ_1_1, n_filters=self.num_filters,
                                                                     filter_size=3, is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='1_2')
-            pooling_1 = tf.layers.max_pooling2d(activ_1_2, pool_size=2, strides=2, padding='same', name='pooling_1')
-            self.nets.extend([conv_1_1, conv_1_2])
+            activ_1_2 = self.nonlin_f(batch_1_2, name='activation_1_2')
+            pooling_1 = tf.layers.max_pooling2d(activ_1_2, pool_size=2, strides=(2, 2), padding='valid', name='pooling_1')
 
         with tf.name_scope('s_conv_2'):
-            conv_2_1, batch_2_1, activ_2_1 = self._conv_bn_layer_tf(pooling_1, n_filters=self.num_filters, filter_scale=2,
+            conv_2_1, batch_2_1, activ_2_1 = self._conv_bn_layer_tf(pooling_1, n_filters=self.num_filters,
                                                                     filter_size=3, is_training=self.is_training,
-                                                                    nonlin_f=self.nonlin_f, name_postfix='2_1')
+                                                                    filter_scale=2, nonlin_f=self.nonlin_f,
+                                                                    name_postfix='2_1')
+            activ_2_1 = self.nonlin_f(batch_2_1, name='activation_2_1')
             conv_2_2, batch_2_2, activ_2_2 = self._conv_bn_layer_tf(activ_2_1, n_filters=self.num_filters,
-                                                                    filter_scale=2,
-                                                                    filter_size=3, is_training=self.is_training,
+                                                                    filter_scale=2, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='2_2')
-            pooling_2 = tf.layers.max_pooling2d(activ_2_2, pool_size=2, strides=2, padding='same', name='pooling_2')
-            self.nets.extend([conv_2_1, conv_2_2])
+            activ_2_2 = self.nonlin_f(batch_2_2, name='activation_2_2')
+            pooling_2 = tf.layers.max_pooling2d(activ_2_2, pool_size=2, strides=(2, 2), padding='valid', name='pooling_2')
 
         with tf.name_scope('s_conv_3'):
-            conv_3_1, batch_3_1, activ_3_1 = self._conv_bn_layer_tf(pooling_2, n_filters=self.num_filters, filter_scale=4,
-                                                                    filter_size=3, is_training=self.is_training,
+            conv_3_1, batch_3_1, activ_3_1 = self._conv_bn_layer_tf(pooling_2, n_filters=self.num_filters,
+                                                                    filter_scale=4, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='3_1')
+            activ_3_1 = self.nonlin_f(batch_3_1, name='activation_3_1')
             conv_3_2, batch_3_2, activ_3_2 = self._conv_bn_layer_tf(activ_3_1, n_filters=self.num_filters,
-                                                                    filter_scale=4,
-                                                                    filter_size=3, is_training=self.is_training,
+                                                                    filter_scale=4, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='3_2')
-            conv_3_3, batch_3_3, activ_3_3 = self._conv_bn_layer_tf(conv_3_2, n_filters=self.num_filters, filter_scale=4,
-                                                                    filter_size=3, is_training=self.is_training,
+            activ_3_2 = self.nonlin_f(batch_3_2, name='activation_3_2')
+            conv_3_3, batch_3_3, activ_3_3 = self._conv_bn_layer_tf(activ_3_2, n_filters=self.num_filters,
+                                                                    filter_scale=4, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='3_3')
-            pooling_3 = tf.layers.max_pooling2d(activ_3_3, pool_size=2, strides=2, padding='same', name='pooling_3')
-
-            self.nets.extend([conv_3_1, conv_3_2, conv_3_3])
+            activ_3_3 = self.nonlin_f(batch_3_3, name='activation_3_3')
+            pooling_3 = tf.layers.max_pooling2d(activ_3_3, pool_size=2, strides=(2, 2), padding='valid', name='pooling_3')
 
         with tf.name_scope('s_conv_4'):
-            conv_4_1, batch_4_1, activ_4_1 = self._conv_bn_layer_tf(pooling_3, n_filters=self.num_filters, filter_scale=8,
-                                                                    filter_size=3, is_training=self.is_training,
+            conv_4_1, batch_4_1, activ_4_1 = self._conv_bn_layer_tf(pooling_3, n_filters=self.num_filters,
+                                                                    filter_scale=8, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='4_1')
+            activ_4_1 = self.nonlin_f(batch_4_1, name='activation_4_1')
             conv_4_2, batch_4_2, activ_4_2 = self._conv_bn_layer_tf(activ_4_1, n_filters=self.num_filters,
-                                                                    filter_scale=8,
-                                                                    filter_size=3, is_training=self.is_training,
+                                                                    filter_scale=8, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='4_2')
-            conv_4_3, batch_4_3, activ_4_3 = self._conv_bn_layer_tf(conv_4_2, n_filters=self.num_filters, filter_scale=8,
-                                                                  filter_size=3, is_training=self.is_training,
+            activ_4_2 = self.nonlin_f(batch_4_2, name='activation_4_2')
+            conv_4_3, batch_4_3, activ_4_3 = self._conv_bn_layer_tf(activ_4_2, n_filters=self.num_filters,
+                                                                    filter_scale=8, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='4_3')
-            pooling_4 = tf.layers.max_pooling2d(activ_4_3, pool_size=2, strides=2, padding='same', name='pooling_4')
-
-            self.nets.extend([conv_4_1, conv_4_2, conv_4_3])
+            activ_4_3 = self.nonlin_f(batch_4_3, name='activation_4_3')
+            pooling_4 = tf.layers.max_pooling2d(activ_4_3, pool_size=2, strides=(2, 2), padding='valid', name='pooling_4')
 
         with tf.name_scope('s_conv_5'):
-            conv_5_1, batch_5_1, activ_5_1 = self._conv_bn_layer_tf(pooling_4, n_filters=self.num_filters, filter_scale=8,
-                                                                    filter_size=3, is_training=self.is_training,
+            conv_5_1, batch_5_1, activ_5_1 = self._conv_bn_layer_tf(pooling_4, n_filters=self.num_filters,
+                                                                    filter_scale=8, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='5_1')
+            activ_5_1 = self.nonlin_f(batch_5_1, name='activation_5_1')
             conv_5_2, batch_5_2, activ_5_2 = self._conv_bn_layer_tf(activ_5_1, n_filters=self.num_filters,
-                                                                    filter_scale=8,
-                                                                    filter_size=3, is_training=self.is_training,
+                                                                    filter_scale=8, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='5_2')
-            conv_5_3, batch_5_3, activ_5_3 = self._conv_bn_layer_tf(conv_5_2, n_filters=self.num_filters, filter_scale=8,
-                                                                    filter_size=3, is_training=self.is_training,
+            activ_5_2 = self.nonlin_f(batch_5_2, name='activation_5_2')
+            conv_5_3, batch_5_3, activ_5_3 = self._conv_bn_layer_tf(activ_5_2, n_filters=self.num_filters,
+                                                                    filter_scale=8, filter_size=3,
+                                                                    is_training=self.is_training,
                                                                     nonlin_f=self.nonlin_f, name_postfix='5_3')
-            pooling_5 = tf.layers.max_pooling2d(activ_5_3, pool_size=2, strides=2, padding='same', name='pooling_5')
-
-            self.nets.extend([conv_5_1, conv_5_2, conv_5_3])
+            activ_5_3 = self.nonlin_f(batch_5_3, name='activation_5_3')
+            pooling_5 = tf.layers.max_pooling2d(activ_5_3, pool_size=2, strides=(2, 2), padding='valid', name='pooling_5')
 
         with tf.name_scope('s_outputs'):
             flat = tf.layers.flatten(pooling_5, name='flatten')
             drop_5 = tf.layers.dropout(flat, rate=self.dropout, name='drop_5')
-            fc_1 = tf.layers.dense(drop_5, units=64, activation=self.nonlin_f, name='fc_1')
+            fc_1 = tf.layers.dense(drop_5, units=256, activation=self.nonlin_f, name='fc_1')
             drop_6 = tf.layers.dropout(fc_1, rate=self.dropout, name='drop_6')
-            fc_2 = tf.layers.dense(drop_6, units=64, activation=self.nonlin_f, name='fc_2')
+            fc_2 = tf.layers.dense(drop_6, units=256, activation=self.nonlin_f, name='fc_2')
             output = tf.layers.dense(fc_2, units=self.num_classes, activation='softmax', name='output')
-            #self.nets.append(output)
         return output
 
 
@@ -185,9 +195,15 @@ class VGG16_pt(NetworkBase, nn.Module):
         self.pool_5 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Output
-        self.fc_1 = nn.Linear(512, 64)
-        self.fc_2 = nn.Linear(64, 64)
-        self.out = nn.Linear(64, self.num_classes)
+        self.out = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, self.num_classes)
+        )
 
     def forward(self, X):
         x = self.conv_1_1(X)
@@ -208,16 +224,13 @@ class VGG16_pt(NetworkBase, nn.Module):
         x = self.conv_4_3(x)
         x = self.pool_4(x)
 
-        #Remove conv 5 for MNIST
-        #x = self.conv_5_1(x)
-        #x = self.conv_5_2(x)
-        #x = self.conv_5_3(x)
-        #x = self.pool_5(x)
+        x = self.conv_5_1(x)
+        x = self.conv_5_2(x)
+        x = self.conv_5_3(x)
+        x = self.pool_5(x)
 
         x = x.view(x.size(0), -1)
 
-        x = self.fc_1(x)
-        x = self.fc_2(x)
         x = self.out(x)
 
         return x
